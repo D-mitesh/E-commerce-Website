db1ce6db02fe6e1c23b44549ea78b5760a329fd3
Miteshnable <miteshnable@gmail.com>
2024-05-20 09:55:40 +0530

    sh2

--- scripts/stop_docker_server.sh
+++ scripts/stop_docker_server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo docker ps -a
 echo "Your shell is currently working in '$(pwd)'"
 cd /home/ubuntu/
-sudo docker-compose down
-sudo docker ps -a
+echo "ubuntu@" | sudo -S docker-compose down -d
+echo "ubuntu@" | sudo -S docker ps -a
